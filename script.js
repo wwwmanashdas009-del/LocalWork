c1b4f97e3251676fe2925118ff7d86332e76a459
wwwmanashdas009-del <www.manashdas009@gmail.com>
2026-09-17 20:19:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBIjUlpWwSGsZK8WzEeNYMgH8qG3tamyek",
   authDomain: "localwork-f6460.firebaseapp.com",
@@ -15,6 +21,9 @@ const firebaseConfig = {
   messagingSenderId: "738787718967",
   appId: "1:738787718967:web:1c5abb9d77528c8b854cb2"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase
 
